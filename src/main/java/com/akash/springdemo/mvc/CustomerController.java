502595076b6ac4783918fc4e93d235e976e232bb
akashmishra <aakashmishraam5@gmail.com>
2023-09-16 12:08:57 +0530

    spring thymeleaf with form validation

--- src/main/java/com/akash/springdemo/mvc/CustomerController.java
+++ src/main/java/com/akash/springdemo/mvc/CustomerController.java
@@ -2,15 +2,26 @@ package com.akash.springdemo.mvc;
 
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    //add a initBinder to covert trim input string to remove whitespaces
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+
+        StringTrimmerEditor stringTrimmerEditor =new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel){
@@ -22,7 +33,10 @@ public class CustomerController {
         @PostMapping("/processForm")
                 public String processForm(
                         @Valid @ModelAttribute("customer") Customer theCustomer,
-                BindingResult theBindingResult)         {
+                BindingResult theBindingResult) {
+
+            System.out.println("Last name: |" + theCustomer.getLastName());
+
         if(theBindingResult.hasErrors()){
             return "customer-form";
         }
