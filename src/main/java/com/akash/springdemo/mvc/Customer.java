93c55e8e857364fdd90571af1b3e3a02a02b39ec
akashmishra <aakashmishraam5@gmail.com>
2023-09-16 13:19:51 +0530

    spring thymeleaf with form validation

--- src/main/java/com/akash/springdemo/mvc/Customer.java
+++ src/main/java/com/akash/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.akash.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,18 @@ public class Customer {
     @NotNull(message = "required")
     @Size(min =1 , message = "required")
     private String LastName= "";
+
+
+    public int getFreePass() {
+        return FreePass;
+    }
+
+    public void setFreePass(int FreePass) {
+        this.FreePass = FreePass;
+    }
+@Min(value = 1 ,message = "must be greater than this")
+@Max(value = 10,message = "must be less than this")
+    private int FreePass;
 
     public String getFirstName() {
         return FirstName;
